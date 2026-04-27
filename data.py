893d1fc371b182d55aef1e8c07aaf19f8ebd90db
Christian Williams <seendub@yahoo.com>
2026-04-27 15:54:05 -0500

    Project 7 submission

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # File with constants
+URBAN_ROUTES_URL = "https://cnt-2b420930-8904-44ac-a5d1-d2d17a6bb867.containerhub.tripleten-services.com"
+ADDRESS_FROM = "East 2nd Street, 601"
+ADDRESS_TO = "1300 1ST ST"
+PHONE_NUMBER = "+1 123 123 1212"
+CARD_NUMBER = "1234 5678 9100"
+CARD_CODE = "1111"
+MESSAGE_FOR_DRIVER = "Stop at the juice bar, please"
