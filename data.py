5412030d306c8f5d89892a1a1925ed9820695bcd
Christian Williams <seendub@yahoo.com>
2026-04-27 21:42:30 -0500

    Project 7 submission (#5)  re-commit main.py & data.py

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 # File with constants
 URBAN_ROUTES_URL = "https://cnt-2d876955-c74b-42a2-aea6-2535435482e9.containerhub.tripleten-services.com"
 ADDRESS_FROM = "East 2nd Street, 601"
-ADDRESS_TO = "1300 1ST ST"
-PHONE_NUMBER = "+1 123 123 1212"
+ADDRESS_TO = "1300 1ST St"
+PHONE_NUMBER = "+1 123 123 12 12"
 CARD_NUMBER = "1234 5678 9100"
 CARD_CODE = "1111"
 MESSAGE_FOR_DRIVER = "Stop at the juice bar, please"
